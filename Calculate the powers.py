88dedc4e9769f79b793e60bc843ad0bfc3d91af0
TheGamerPro-dev <73950364+TheGamerPro-dev@users.noreply.github.com>
2022-03-01 12:23:34 +0530

    Add files via upload

---
+++ Calculate the powers.py
@@ -0,0 +1,4 @@
+number = int(input("Enter the base integer here: "))
+exponent = int(input("Enter the exponent integer here: "))
+answer = number**exponent
+print(f"""The answer is as follows: {answer}""")
